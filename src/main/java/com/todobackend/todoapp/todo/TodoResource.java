43666455b497bebef78be5ef9b1e764e12fb7bf1
anand-yv <anand93056ad@gmail.com>
2024-02-28 13:22:21 +0530

    Added where user can get there task and delete there task by id implemented using GET and and DELETE

--- src/main/java/com/todobackend/todoapp/todo/TodoResource.java
+++ src/main/java/com/todobackend/todoapp/todo/TodoResource.java
@@ -1,8 +1,6 @@
 package com.todobackend.todoapp.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,4 +17,16 @@ public class TodoResource {
     public List<Todo> retrieveTodos(@PathVariable String username){
         return todoService.findByUsername(username);
     }
+
+    @GetMapping(path = "/users/{username}/todos/{id}")
+    public Todo retrieveTodoById(@PathVariable String username ,@PathVariable Integer id){
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping(path = "/users/{username}/todos/{id}")
+    public void deleteTodoById(@PathVariable String username, @PathVariable Integer id){
+        todoService.deleteById(id);
+    }
+
+
 }
